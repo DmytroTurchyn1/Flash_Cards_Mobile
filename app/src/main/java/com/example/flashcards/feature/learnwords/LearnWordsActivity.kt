3bc35c9644ff87914e87b9dca518f8ca8463e138
leogluck <leogluck@gmail.com>
2021-12-22 21:04:09 +0200

    save

--- app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsActivity.kt
+++ app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsActivity.kt
@@ -17,7 +17,7 @@ class LearnWordsActivity : AppCompatActivity(), LearnWordsView {
         presenter.onActivityCreated()
 
         ibMenu.setOnClickListener { presenter.onMenuBtnClicked() }
-        btnShowNativeWord.setOnClickListener { presenter.onShowEnlishWordClicked() }
+        btnShowNativeWord.setOnClickListener { presenter.onShowEnglishWordClicked() }
         btnNext.setOnClickListener { presenter.onNextButtonClicked() }
     }
 
@@ -35,39 +35,4 @@ class LearnWordsActivity : AppCompatActivity(), LearnWordsView {
     override fun showEnglishWord(englishWord: String) {
         tvEnglishWord.text = englishWord
     }
-
-  //  open var id: Int = +1
-
-    //fun next(view: android.view.View) {
-//        try {
-//            var ukr_word = findViewById<TextView>(R.id.ukrWord)
-//            ukr_word.setVisibility(View.VISIBLE)
-//            id = +1
-//            val words = realm.where(Word::class.java).equalTo("id", id).findAll()
-//            words.forEach { word ->
-//                ukr_word.text = word.nativeWord
-//
-//            }
-//        } catch (e: Exception) {
-//            val error =
-//                Toast.makeText(applicationContext, "There is an error!", Toast.LENGTH_SHORT)
-//            error.show()
-//        }
-  //  }
- //       fun see_en_form(view: android.view.View) {
-//            try {
-//
-//                var en_word = findViewById<TextView>(R.id.enWord)
-//                val words = realm.where(Word::class.java).equalTo("id", id).findAll()
-//                words.forEach { word ->
-//                    en_word.text = word.englishWord
-//
-//                }
-//            } catch (e: Exception) {
-//                val error =
-//                    Toast.makeText(applicationContext, "There is an error!", Toast.LENGTH_SHORT)
-//                error.show()
-//            }
- //       }
-
 }
--- app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsPresenter.kt
+++ app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsPresenter.kt
@@ -11,24 +11,17 @@ class LearnWordsPresenter(view: LearnWordsView) {
 
     private val wordsRepository = WordsRepository.getInstance()
     private var words = listOf<Word>()
-    open var id: Int = +1
+    private var id: Int = 1
+
     fun onActivityCreated() {
         words = wordsRepository.getWords()
     }
 
-    fun onActivityStarted() {
-        //show first word on screen
-        view?.showNativeWord(words[id].nativeWord)
-    }
+    fun onActivityStarted() = view?.showNativeWord(words[id].nativeWord)
 
-    fun onShowEnlishWordClicked() {
-        //show first word on screen
-        view?.showEnglishWord(words[id].englishWord)
-    }
-    fun onNextButtonClicked() {
-        //show first word on screen
-        view?.showNativeWord(words[id].nativeWord)
-    }
+    fun onShowEnglishWordClicked() = view?.showEnglishWord(words[id].englishWord)
+
+    fun onNextButtonClicked() = view?.showNativeWord(words[id].nativeWord)
+
     fun onMenuBtnClicked() = view?.navigateToMenuActivity()
-
 }

babc530fadb1f6dd6b2653e346f205831e6d3369
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2021-12-30 18:03:02 +0200

    Random words (#2)
    * save
    
    * save
    
    * save
    
    * save
    
    * Done
    
    * save
    
    Co-authored-by: leogluck <leogluck@gmail.com>

--- app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsPresenter.kt
+++ app/src/main/java/com/example/flashcards/feature/learnwords/LearnWordsPresenter.kt
@@ -3,6 +3,8 @@ package com.example.flashcards.feature.learnwords
 import com.example.flashcards.model.local.Word
 import com.example.flashcards.repository.WordsRepository
 import java.lang.ref.WeakReference
+import kotlin.random.Random
+import io.realm.annotations.PrimaryKey
 
 class LearnWordsPresenter(view: LearnWordsView) {
     private val viewReference = WeakReference(view)
@@ -11,17 +13,28 @@ class LearnWordsPresenter(view: LearnWordsView) {
 
     private val wordsRepository = WordsRepository.getInstance()
     private var words = listOf<Word>()
-    private var id: Int = 1
+    private var id: Int = getRandomId()
 
     fun onActivityCreated() {
         words = wordsRepository.getWords()
     }
 
-    fun onActivityStarted() = view?.showNativeWord(words[id].nativeWord)
+    fun onActivityStarted() = showNativeWord()
+
+    private fun showNativeWord() = view?.showNativeWord(words[id].nativeWord)
 
     fun onShowEnglishWordClicked() = view?.showEnglishWord(words[id].englishWord)
 
-    fun onNextButtonClicked() = view?.showNativeWord(words[id].nativeWord)
+    fun onNextButtonClicked() {
+        updateWordId()
+        showNativeWord()
+    }
+
+    private fun updateWordId() {
+        id = getRandomId()
+    }
 
     fun onMenuBtnClicked() = view?.navigateToMenuActivity()
+
+    private fun getRandomId() = Random.nextInt(0, words.size)
 }

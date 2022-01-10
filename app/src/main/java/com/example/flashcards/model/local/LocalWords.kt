2a8e50c64673f02adbb6eb8fe74c2a56f7add0a0
dimaturchyn <dim4ukt@gmail.com>
2022-01-10 19:30:30 +0200

    save

--- app/src/main/java/com/example/flashcards/model/local/LocalWords.kt
+++ app/src/main/java/com/example/flashcards/model/local/LocalWords.kt
@@ -3,5 +3,9 @@ package com.example.flashcards.model.local
 import io.realm.Realm
 
 object IrregularVerbsRealm : Realm {
-
+    fun prymarykey(){}
+    fun Native(){}
+    fun FirstForm(){}
+    fun SecondForm(){}
+    fun ThirdForm(){}
 }

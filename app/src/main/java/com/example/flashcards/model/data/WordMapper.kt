0b698632cafdc55f6bebbfb35e3d6f2338137217
dimaturchyn <dim4ukt@gmail.com>
2022-01-17 23:30:19 +0200

    done

--- app/src/main/java/com/example/flashcards/model/data/WordMapper.kt
+++ app/src/main/java/com/example/flashcards/model/data/WordMapper.kt
@@ -6,7 +6,7 @@ import com.example.flashcards.model.local.SimpleWord
 import io.realm.RealmResults
 
 class WordMapper {
-    fun mapSimpleWordsRealm(words: RealmResults<UserWordRealm>) =
+    fun mapNewWordsRealm(words: RealmResults<UserWordRealm>) =
         words.map { simpleWord -> UserWord(nativeWord = simpleWord.nativeWord, englishWord = simpleWord.englishWord) }
 
     fun mapIrregularVerbsRealm(words: RealmResults<IrregularVerbRealm>): List<IrregularVerb> {
@@ -14,9 +14,9 @@ class WordMapper {
             IrregularVerb(irregularVerb.nativeWord, irregularVerb.firstForm, irregularVerb.secondForm, irregularVerb.thirdForm)
         }
     }
-    fun mapNewWordsRealm(words: RealmResults<SimpleWordsRealm>): List<SimpleWord> {
-        return words.map { newwords ->
-            SimpleWord(newwords.nativeWord, newwords.englishWord)
+    fun mapSimpleWordsRealm (words: RealmResults<SimpleWordsRealm>): List<SimpleWord> {
+        return words.map { simplewords ->
+            SimpleWord(simplewords.nativeWord, simplewords.englishWord)
         }
     }
 }

3002e392c100bb39b95bbb1cce3925b3c6f38179
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2023-09-03 22:02:41 +0200

    bugs fixed, notifications added, update check added and more

--- app/src/main/java/com/app/flashcards/feature/aboutme/AboutMeActivity.kt
+++ app/src/main/java/com/app/flashcards/feature/aboutme/AboutMeActivity.kt
@@ -1,16 +1,11 @@
 package com.app.flashcards.feature.aboutme
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.app.flashcards.BuildConfig
-
-
-import com.app.flashcards.util.Launcher
-import com.app.flashcards.R
 import com.app.flashcards.databinding.ActivityAboutMeBinding
-import com.app.flashcards.databinding.ActivityMainBinding
+import com.app.flashcards.util.Launcher
 
 
 class AboutMeActivity : AppCompatActivity(), AboutMeView {
@@ -21,12 +16,13 @@ class AboutMeActivity : AppCompatActivity(), AboutMeView {
         binding = ActivityAboutMeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         presenter = AboutMePresenter(this)
-
+        println("hello!")
         binding.btnMenu.setOnClickListener { presenter.onMenuBtnClicked() }
         binding.btnGmail.setOnClickListener { presenter.onGmailBtnClicked() }
         binding.btnYoutube.setOnClickListener { presenter.onYoutubeBtnClicked() }
         binding.btnTelegram.setOnClickListener { presenter.onTelegramBtnClicked() }
         binding.tvVersion.text = "Version: " + BuildConfig.VERSION_NAME
+
     }
     override fun navigateToMenuActivity() = Launcher.startMenuActivity(this)
 
--- app/src/main/java/com/app/flashcards/feature/main/MainView.kt
+++ app/src/main/java/com/app/flashcards/feature/main/MainView.kt
@@ -3,4 +3,6 @@ package com.app.flashcards.feature.main
 interface MainView {
     fun navigateToMenuActivity()
     fun initAdmob()
+    fun get_token()
+    fun askNotificationPermission()
 }

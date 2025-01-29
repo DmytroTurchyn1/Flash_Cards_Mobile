f369f610960629d247f826424df1b1089f92b60d
Dmytro <dim4ukt@gmail.com>
2025-01-29 17:26:44 +0100

    Remove debug print statement from MyFirebaseMessagingService

--- app/src/main/java/com/app/flashcards/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/app/flashcards/MyFirebaseMessagingService.kt
@@ -11,7 +11,7 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        println("Received message: $remoteMessage")
+//        println("Received message: $remoteMessage")
     }
 
 }

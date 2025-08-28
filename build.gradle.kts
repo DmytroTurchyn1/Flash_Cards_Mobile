675904f6bf3cca0b79b75913859df3e7603bf194
Dmytro <dim4ukt@gmail.com>
2025-08-28 15:29:05 +0200

    update dependencies and configuration files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.12.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("io.realm:realm-gradle-plugin:10.19.0")
         classpath("com.google.gms:google-services:4.4.3")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.4")
-        classpath("com.google.firebase:perf-plugin:1.4.2")
-        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.6")
+        classpath("com.google.firebase:perf-plugin:2.0.1")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.10")
     }
 }
